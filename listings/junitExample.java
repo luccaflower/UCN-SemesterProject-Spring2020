aee76c0f7824ce0ce9d534e357bb4db878c6a47f
Lucca Christiansen <rkaasgaard@gmail.com>
2020-05-25 09:43:54 +0200

    changed code listings to use minted added reference to aaa pattern rewrote some testing thingies

---
+++ listings/junitExample.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+
+public class TestClass {
+    
+    @BeforeAll
+    public static void setupAll() {
+    }
+
+    @BeforeEach
+    public void setupEach() {
+    }
+    
+    @Test
+    public void testMethod() {
+    }
+
+    @AfterEach
+    public void tearDownEach() {
+    }
+
+    @AfterAll
+    public static void tearDownAll() {
+    }
+}
