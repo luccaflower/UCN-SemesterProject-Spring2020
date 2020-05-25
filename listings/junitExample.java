66fe199f70e727fe4eb534ad5075c5dccbc27333
Lucca Christiansen <rkaasgaard@gmail.com>
2020-05-25 10:22:37 +0200

    removed import statements from juniexample listing

--- listings/junitExample.java
+++ listings/junitExample.java
@@ -1,9 +1,3 @@
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-
 public class TestClass {
     
     @BeforeAll
