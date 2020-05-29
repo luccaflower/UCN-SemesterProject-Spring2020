4f590352fb8b6e17d0b27b8e8ef230f9faafacc5
Alexandra Østermark <alex.cramt@gmail.com>
2020-05-29 16:30:37 +0200

    its now a little faster

--- scripts/get_char_count.rb
+++ scripts/get_char_count.rb
@@ -1,15 +1,18 @@
 require "json"
 require "pdf-reader"
 
-latex_file = "small_master"
+if ENV["EXEC"] == "Y"
+    latex_file = "small_master"
 
-command = JSON.parse(File.read(".vscode/settings.json"))["latex-workshop.latex.tools"].find {|x| x["name"] == "pdflatex"}
-args = command["args"]
-command = command["command"]
-args = args.map {|x| x == "%DOC%" ? latex_file + ".tex" : x}
-`#{command} #{args.join(" ")}`
+    command = JSON.parse(File.read(".vscode/settings.json"))["latex-workshop.latex.tools"].find {|x| x["name"] == "pdflatex"}
+    args = command["args"]
+    command = command["command"]
+    args = args.map {|x| x == "%DOC%" ? latex_file + ".tex" : x}
+    `#{command} #{args.join(" ")}`
 
-pdf = PDF::Reader.new(latex_file + ".pdf")
-char_count = pdf.pages.map {|x| x.text}.join(" ").gsub(/\s+/, " ").length
-File.write("current_char_count.tmp", char_count)
-puts char_count
+    pdf = PDF::Reader.new(latex_file + ".pdf")
+    char_count = pdf.pages.map {|x| x.text}.join(" ").gsub(/\s+/, " ").length
+    File.write("current_char_count.tmp", char_count)
+else
+    Process.spawn({"EXEC" => "Y"}, "ruby " + __FILE__)
+end
