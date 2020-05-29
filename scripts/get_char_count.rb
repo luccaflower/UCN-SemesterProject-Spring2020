35d9245831cb4b40abaecb76e19738f8931d2883
Alexandra Østermark <alex.cramt@gmail.com>
2020-05-29 15:59:09 +0200

    scripts for char

--- scripts/get_char_count.rb
+++ scripts/get_char_count.rb
@@ -1,7 +1,7 @@
 require "json"
 require "pdf-reader"
 
-latex_file = "master"
+latex_file = "small_master"
 
 command = JSON.parse(File.read(".vscode/settings.json"))["latex-workshop.latex.tools"].find {|x| x["name"] == "pdflatex"}
 args = command["args"]
@@ -10,4 +10,6 @@ args = args.map {|x| x == "%DOC%" ? latex_file + ".tex" : x}
 `#{command} #{args.join(" ")}`
 
 pdf = PDF::Reader.new(latex_file + ".pdf")
-puts pdf.pages.map {|x| x.text}.join(" ").gsub(/\s+/, " ").length
+char_count = pdf.pages.map {|x| x.text}.join(" ").gsub(/\s+/, " ").length
+File.write("current_char_count.tmp", char_count)
+puts char_count
